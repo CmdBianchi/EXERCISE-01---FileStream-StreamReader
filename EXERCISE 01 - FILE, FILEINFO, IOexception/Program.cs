592a7182d93023e7262e483b8abf742a5a6a6833
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-21 15:48:21 -0300

    V.2 FORMA MAIS RESUMIDA

--- EXERCISE 01 - FILE, FILEINFO, IOexception/Program.cs
+++ EXERCISE 01 - FILE, FILEINFO, IOexception/Program.cs
@@ -6,11 +6,9 @@ namespace EXERCISE_01___FILE__FILEINFO__IOexception {
     class Program {
         static void Main(string[] args) {
             string path = @"c:\temp\file1.txt";
-            FileStream fs = null;
             StreamReader sr = null;
             try {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
+                sr = File.OpenText(path);
                 string line = sr.ReadLine();
                 Console.WriteLine(line);
             }
@@ -21,7 +19,6 @@ namespace EXERCISE_01___FILE__FILEINFO__IOexception {
             }
             finally {
                 if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
             }
         }
     }
