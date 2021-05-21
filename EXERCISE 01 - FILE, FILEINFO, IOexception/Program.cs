d02efea4fe6602eafb9f58ca73b157c4cb943abc
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-21 15:40:19 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - FILE, FILEINFO, IOexception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace EXERCISE_01___FILE__FILEINFO__IOexception {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(IOException e) {
+                Console.WriteLine("An error aciirred");
+                Console.WriteLine(e.Message);
+                
+            }
+            finally {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
